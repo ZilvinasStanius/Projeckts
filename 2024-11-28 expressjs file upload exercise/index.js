960c596d74470bf7cf0064da666d040e6b0dc7ec
ZilvinasStanius <zilvinas950203@gmail.com>
2024-12-02 12:06:12 +0200

    some updates

--- 2024-11-28 expressjs file upload exercise/index.js
+++ 2024-11-28 expressjs file upload exercise/index.js
@@ -8,16 +8,12 @@ import bodyParser from 'body-parser';
 import session from 'express-session';
 import dotenv from 'dotenv';
 import { readdir } from 'node:fs/promises';
-
+//____________
 dotenv.config();
 const app = express();
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//publik file directory
-const publicFilesPath = path.join(root, 'public');
-/// private file directory
-const privateFilesPath = path.join(root, 'private');
-// setting up session
+//SEASION
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -25,12 +21,29 @@ app.use(
     saveUninitialized: true,
   })
 );
-//____________________________
+//publik file directory
+const publicFilesPath = path.join(root, 'public');
+/// private file directory
+const privateFilesPath = path.join(root, 'private');
+// Pages file directory
+const staticPublicPath = path.join(root, 'pages');
+
+const stylePath = path.join(root, 'styles');
+
+//STATIC FILE USING_______________
 app.use('/public', express.static(publicFilesPath));
 app.use('/private', express.static(privateFilesPath));
-//_____________________________
+app.use('/styles', express.static(stylePath));
+//ROUTES_____________________________
 
-const staticPublicPath = path.join(root, 'pages');
+//get User
+app.get('/user', (req, res) => {
+  const username = req.session.user;
+  if (!username) {
+    return res.status(401).json({ message: 'Unautorized' });
+  }
+  res.json({ username });
+});
 
 //Getting all files :
 app.get('/all-files', async (req, res) => {
@@ -75,17 +88,20 @@ app.get('/public/all-files', async (req, res) => {
   res.json(pblFiles);
 });
 
-//Login page ________________________
+// Login page ________________________
 app.get('/', (req, res) => {
   res.sendFile(path.join(staticPublicPath, 'login.html'));
 });
-
+//Public files page______________________________
 app.get('/public', (req, res) => {
   res.sendFile(path.join(staticPublicPath, 'public.html'));
 });
+//Registration page______________________
 app.get('/register', (req, res) => {
   res.sendFile(path.join(staticPublicPath, 'register.html'));
 });
+
+//Protectred page route___________________
 app.get('/protected', (req, res) => {
   const usr = req.session.user;
   if (!usr) {
@@ -94,7 +110,7 @@ app.get('/protected', (req, res) => {
   console.log('User logged in with username:', usr);
   res.sendFile(path.join(staticPublicPath, 'protected.html'));
 });
-
+//File add page_________________________
 app.get('/file-add', (req, res) => {
   const usr = req.session.user;
   if (!usr) {
@@ -102,32 +118,49 @@ app.get('/file-add', (req, res) => {
   }
   res.sendFile(path.join(staticPublicPath, 'add-file.html'));
 });
-
+//Seasion destroy "LOGOUT" route_____________
 app.get('/logout', (req, res) => {
   req.session.destroy();
   res.redirect('/');
 });
-// trying to log in
+// LogIn route__________________________
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
     const user = await User.findOne({ where: { username } });
     if (!user) {
-      return res.status(400).send('User not found');
+      return res.status(400).json({ error: 'User not found' });
     }
     if (user.password !== password) {
-      return res.status(400).send('Incorrect password');
+      return res.status(400).json({ error: 'Incorrect password' });
     }
-    //Store user ID in seasion
+    //Store username in session
     req.session.user = user.username;
-    res.redirect('/protected');
+    // res.redirect('/protected');
+    res.status(200).json({ message: 'Login successful!' });
   } catch (err) {
     console.log(err);
     res.status(500).send('error logging in');
   }
 });
-
+//Registration route________________________
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    //checking if user already exist
+    const existingUser = await User.findOne({ where: { username } });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exist' });
+    }
+    // create user
+    const newUser = await User.create({ username, password });
+    res.status(200).json({ message: 'User created' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Error registering user' });
+  }
+});
 //__________multer
 const storage = multer.diskStorage({
   destination: function async(req, file, cb) {
@@ -145,34 +178,17 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage, dest: path.join(root, '/private') });
-//FILE UPLOAD
 
+//FILE UPLOAD____________
 app.post('/upload', upload.single('file'), (req, res) => {
-  console.log(req.body.public);
-  res.send('file uploaded');
-});
-
-// trying to register user
-app.post('/register', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    //checking if user already exist
-    const existingUser = await User.findOne({ where: { username } });
-    if (existingUser) {
-      // return res.status(400).send('Username already exist');
-
-      return res.redirect('/register');
-    }
-    // create user
-    const newUser = await User.create({ username, password });
-    res.redirect('/');
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Error registering user');
+  if (!req.file) {
+    return res.redirect('/file-add');
+  } else {
+    res.redirect('/file-add');
   }
 });
-//_______________________________________________________
 
+//____________________________________
 await User.sync({ alter: true });
 app.listen(7999, () => {
   console.log('Connected to http://localhost:7999');
